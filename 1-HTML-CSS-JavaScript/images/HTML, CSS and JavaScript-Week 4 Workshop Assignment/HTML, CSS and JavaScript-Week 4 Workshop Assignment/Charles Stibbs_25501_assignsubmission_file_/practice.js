735d790428e4e6fb35676644bca6efa769c22aa8
unknown <todaregreatly@gmail.com>
2020-10-06 21:41:22 -0600

    first commit

---
+++ 1-HTML-CSS-JavaScript/images/HTML, CSS and JavaScript-Week 4 Workshop Assignment/HTML, CSS and JavaScript-Week 4 Workshop Assignment/Charles Stibbs_25501_assignsubmission_file_/practice.js
@@ -0,0 +1,6 @@
+
+let i =0;
+while (i < 5){
+    console.log("loop iteration", i);
+    i +=1;
+}
---
+++ 1-HTML-CSS-JavaScript/js/javascript-hello-world.js
@@ -0,0 +1,8 @@
+function getName(){
+    const userName = prompt("What is Your Name?");
+    sayHello(userName);
+}
+function sayHello(name){
+    alert('Hello '  + name + ' !');
+     console.log("alert sent !");
+}
